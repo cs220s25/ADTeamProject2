14c8d9868647e23b2170ddbf6106624e319be736
Deborah <rabinovichd@moravian.edu>
2025-04-24 14:37:38 -0400

    added env connections

--- startup.sh
+++ startup.sh
@@ -8,9 +8,12 @@ else
     echo "Network trivia already exists."
 fi
 
+docker rm -f redisdb 2</dev/null
+docker rm -f trivia-bot 2>/dev/null
+
 # Start Redis
 docker run -d --network trivia --name redisdb -v $(pwd)/data:/data redis redis-server --save 10 1
 
 # Build and start bot
 docker build -t trivia-bot .
-docker run -d --network trivia --name trivia-bot trivia-bot
+docker run -d --network trivia --name trivia-bot --env-file aws.env trivia-bot
