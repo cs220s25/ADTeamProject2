a95f579ca132717abb87eab5a23e9aef6dd5706e
Amanda McNesby <mcnesbya@moravian.edu>
2025-04-26 12:59:57 -0400

    add documentation

--- userDataDockerEC2.sh
+++ userDataDockerEC2.sh
@@ -1,11 +1,14 @@
 #!/bin/bash
 
+# Install and configure Docker
 yum install -y docker
 systemctl enable docker
 systemctl start docker
+
 # Add ec2-user to the docker group so that it can run docker commands without sudo.
 usermod -a -G docker ec2-user
 
+# Install Git and clone repository
 yum install -y git
 git clone https://github.com/cs220s25/ADTeamProject2.git /ADTeamProject2
 cd /ADTeamProject2
