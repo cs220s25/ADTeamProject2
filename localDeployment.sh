03901cee0c3eaf3eea442b9efd1502d0f09cd94d
Amanda McNesby <mcnesbya@moravian.edu>
2025-04-28 09:57:17 -0400

    add to script

--- localDeployment.sh
+++ localDeployment.sh
@@ -1,7 +1,9 @@
 # Start local redis server 
 brew services start redis
 
+# Build the app
 mvn clean
 mvn package
 
+# Run with Redis config
 REDIS_HOST=localhost REDIS_PORT=6379 java -jar target/dbot-1.0-SNAPSHOT.jar
